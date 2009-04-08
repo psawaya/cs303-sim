b194f38c5e032600d89efe5bfac73475881e252a
jmschanck <jmschanck@5e696d22-23c8-11de-8d76-854d91f79a93>
2009-04-08 02:11:08 +0000

    Loads of changes, added the interpreter. Type help at the prompt for more info

--- trunk/ca.py
+++ trunk/ca.py
@@ -1,74 +1,135 @@
-import pygame, random, sys
+import sys
+import random
+import pyconsole
+import pygame
 from pygame.locals import *
 
+#TODO: Better cli argument handling
 if len(sys.argv) == 2:
     numcells = int(sys.argv[1])
 else:
     numcells = 40
 
+bgcolor = [0x00, 0x00, 0x00] # Background
+bocolor = [0x10, 0x10, 0x10] # Border
+fgcolor = [0x00, 0xff, 0x00] # Foreground
+SCREENW = 600 # Screen Width
+SCREENH = 600 # Screen Height
+
 def main():
-    G_Screen = pygame.display.set_mode((400,400))
-    G_Screen.fill([0xff,0xff,0xff])
-    cwidth = G_Screen.get_width()/numcells
-    cheight = G_Screen.get_height()
+    G_Screen = pygame.display.set_mode((SCREENW,SCREENH))
+    G_Screen.fill(bgcolor)
     board = WolframArray(numcells)
-    display = CADisplay(G_Screen, board, fancy=True)
-    execSpeed = 100
+    D_Screen = pygame.Surface((SCREENW, SCREENH))
+    display = CADisplay(D_Screen, board, fancy=True)
+    C_Screen = pygame.Surface((SCREENW, SCREENH/2))
+    console = pyconsole.Console(C_Screen,
+                                (0,0,SCREENW, SCREENH/2),
+                                functions={"+":board.setAlive,
+                                           "-":board.setDead,
+                                           "print":board.toString,
+                                           "rand":board.randomize,
+                                           "stop":display.stop,
+                                           "start":display.run,
+                                           "step":display.step},
+                                key_calls={"d":sys.exit})
+    delay = 100
     while 1:
+        console.process_input()
+        #Handle Events
         for event in pygame.event.get():
+            #Keypresses
             if event.type == KEYDOWN:
+                # q quits
                 if event.key == K_q:
                     pygame.event.post(pygame.event.Event(QUIT))
+                # w toggles the console
+                elif event.key == K_w:
+                    console.set_active()
+                # r randomizes the display
                 elif event.key == K_r:
                     board.randomize()
+                # 1 sets the sim speed to slow
                 elif event.key == K_1:
-                    execSpeed = 10
+                    delay = 200
+                # 2 sets the sim speed to medium 
                 elif event.key == K_2:
-                    execSpeed = 100
+                    delay = 100
+                # 1 sets the sim speed to /fast/ 
                 elif event.key == K_3:
-                    execSpeed = 200
+                    delay = 10
             elif event.type == QUIT:
                 sys.exit()
+        #Draw the console to its buffer
+        console.draw()
+        #Update the simulation
         display.update()
+        #blit the simulation surface to the root surface
+        G_Screen.blit(D_Screen,(0,0))
+        #blit the console surface to the root surface if it's active
+        if console.active:
+            G_Screen.blit(C_Screen, (0,0))
+        #Display the screen
+        pygame.display.flip()
+        #Wait until next timestep
+        pygame.time.wait(delay)
 
 class CADisplay:
     def __init__(self, screen, board, fancy=True):
         self.screen = screen
         self.board = board
+        #Fancy mode makes the history fade out
         self.fancy = fancy
         if self.fancy:
             self.screen.set_alpha(210)
-        self.screen.fill([0,0,0])
+        self.screen.fill(bgcolor)
         cwidth = screen.get_width()/board.numcells
         cheight = screen.get_height()/board.numcells
         self.histlen = screen.get_height()/cheight
-        self.rects = []
-        #History rects
+        #History rects, Rectangles defining each row on the screen
+        self.histrects = []
         for i in range(self.histlen):
-            self.rects.append(pygame.Rect(0, cheight*i, screen.get_width(), cheight))
+            self.histrects.append(pygame.Rect(0, cheight*i, screen.get_width(), cheight))
         #Current cell rects
         self.cellRects = []
         for col in range(numcells):
             self.cellRects.append(pygame.Rect(col*cwidth, screen.get_height()-cheight, cwidth, cheight))
+        self.running = True
+    def run(self):
+        """Run the simulation"""
+        self.running = True
+    def stop(self):
+        """Stop the simulation"""
+        self.running = False
     def update(self):
+        if not self.running:
+            return
+        self.step()
+    def step(self):
+        """Advance the simulation and display by one timestep"""
         for i in range(self.histlen-1):
-            self.screen.fill([0,0,0], self.rects[i])
-            self.screen.blit(self.screen, self.rects[i], self.rects[i+1])
+            self.screen.fill(bgcolor, self.histrects[i])
+            self.screen.blit(self.screen, self.histrects[i], self.histrects[i+1])
             #Double blit necessary for fancy screen!
             if self.fancy:
-                self.screen.blit(self.screen, self.rects[i], self.rects[i+1])
-        self.screen.fill([0,0,0], self.rects[-1])
+                self.screen.blit(self.screen, self.histrects[i], self.histrects[i+1])
+        #Step the simulation
+        self.board.step()
+        #Clear the bottom row for new data
+        self.screen.fill(bgcolor, self.histrects[-1])
+        #Copy new data into the display
         for col in range(numcells):
             alive = self.board.isAlive(col)
-            pygame.draw.rect(self.screen, (0, 0xff*alive, 0), self.cellRects[col])
-        self.board.step()
-        pygame.display.flip()
-        pygame.time.wait(100)
+            if alive:
+                pygame.draw.rect(self.screen, fgcolor, self.cellRects[col])
+            pygame.draw.rect(self.screen, bocolor, self.cellRects[col], 1)
 
 class WolframArray:
     def __init__(self, numcells):
         self.numcells = numcells
         self.cells = [0]*self.numcells
+    def toString(self):
+        return ''.join(str(i) for i in self.cells)
     def neighborCount(self, row):
         return sum(self.getNeighbors(row))
     def getNeighbors(self, col):
@@ -81,12 +142,15 @@ class WolframArray:
             col = 0
         return self.cells[col]
     def setAlive(self, col):
+        """Turn the specified cell on"""
         self.cells[col] = 1
     def setDead(self, col):
+        """Turn the specified cell on"""
         self.cells[col] = 0
     def isAlive(self, col):
         return bool(self.cells[col])
     def randomize(self):
+        """Randomize the display"""
         for r in range(numcells):
             self.cells[r] = random.randint(0,1)
     def step(self):
